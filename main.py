8411f93b5c03187cff8bd915d63ffa881ff16c05
khauta seabata <75206578+KhautaSeabata@users.noreply.github.com>
2025-05-16 10:08:29 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,31 @@
 from fastapi import FastAPI, WebSocket
-from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
-import asyncio
+from fastapi.staticfiles import StaticFiles
 import aiohttp
+import asyncio
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-
 @app.get("/")
-async def root():
+async def home():
     return FileResponse("static/index.html")
 
-
-@app.websocket("/ws/vix75")
-async def vix75_socket(websocket: WebSocket):
+@app.websocket("/ws/vix25")
+async def vix25_socket(websocket: WebSocket):
     await websocket.accept()
-
-    url = "https://data-364f1-default-rtdb.firebaseio.com/vix75.json"
+    url = "https://data-364f1-default-rtdb.firebaseio.com/vix25.json"
 
     async with aiohttp.ClientSession() as session:
         while True:
             try:
                 async with session.get(url) as resp:
                     data = await resp.json()
-                    if not data:
-                        await asyncio.sleep(1)
-                        continue
-
-                    # Get the latest tick
-                    latest_key = sorted(data.keys())[-1]
-                    tick = data[latest_key]
-
-                    await websocket.send_json({"tick": tick})
-
+                    if data:
+                        latest_key = sorted(data.keys())[-1]
+                        tick = data[latest_key]
+                        await websocket.send_json(tick)
             except Exception as e:
-                print("WebSocket error:", e)
+                print("Error:", e)
                 break
-
             await asyncio.sleep(1)
-
