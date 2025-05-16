6a5ad4e8adb1922b0dce43c2e862b9b3d2b98433
khauta seabata <75206578+KhautaSeabata@users.noreply.github.com>
2025-05-16 10:01:39 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,80 +1,42 @@
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi import FastAPI, WebSocket
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import FileResponse, HTMLResponse
+from fastapi.responses import FileResponse
 import asyncio
-import httpx
-import json
+import aiohttp
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+
 @app.get("/")
-async def index():
-    return HTMLResponse("<h2>Choose a chart:</h2><ul>" +
-                        "<li><a href='/vix10'>Vix10</a></li>" +
-                        "<li><a href='/vix25'>Vix25</a></li>" +
-                        "<li><a href='/vix75'>Vix75</a></li>" +
-                        "<li><a href='/vix100'>Vix100</a></li></ul>")
+async def root():
+    return FileResponse("static/index.html")
 
-@app.get("/vix10")
-async def vix10():
-    return FileResponse("static/chart.html")
 
-@app.get("/vix25")
-async def vix25():
-    return FileResponse("static/chart.html")
+@app.websocket("/ws/vix75")
+async def vix75_socket(websocket: WebSocket):
+    await websocket.accept()
 
-@app.get("/vix75")
-async def vix75():
-    return FileResponse("static/chart.html")
+    url = "https://data-364f1-default-rtdb.firebaseio.com/vix75.json"
 
-@app.get("/vix100")
-async def vix100():
-    return FileResponse("static/chart.html")
-
-FIREBASE_URL = "https://data-364f1-default-rtdb.firebaseio.com"
-
-class ConnectionManager:
-    def __init__(self):
-        self.active_connections: list[WebSocket] = []
-
-    async def connect(self, websocket: WebSocket):
-        await websocket.accept()
-        self.active_connections.append(websocket)
-
-    def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)
-
-    async def send_message(self, websocket: WebSocket, message: str):
-        await websocket.send_text(message)
-
-manager = ConnectionManager()
-
-async def get_latest_tick(symbol: str):
-    async with httpx.AsyncClient() as client:
-        url = f"{FIREBASE_URL}/{symbol}.json"
-        resp = await client.get(url)
-        data = resp.json()
-        if not data:
-            return None
-        return max(data.values(), key=lambda x: x["timestamp"])
-
-@app.websocket("/ws/{symbol}")
-async def websocket_endpoint(websocket: WebSocket, symbol: str):
-    await manager.connect(websocket)
-    try:
+    async with aiohttp.ClientSession() as session:
         while True:
-            tick = await get_latest_tick(symbol)
-            if tick:
-                message = {
-                    "symbol": symbol,
-                    "tick": {
-                        "quote": tick["price"],
-                        "epoch": tick["timestamp"],
-                        "time": tick["time"],
-                    }
-                }
-                await manager.send_message(websocket, json.dumps(message))
+            try:
+                async with session.get(url) as resp:
+                    data = await resp.json()
+                    if not data:
+                        await asyncio.sleep(1)
+                        continue
+
+                    # Get the latest tick
+                    latest_key = sorted(data.keys())[-1]
+                    tick = data[latest_key]
+
+                    await websocket.send_json({"tick": tick})
+
+            except Exception as e:
+                print("WebSocket error:", e)
+                break
+
             await asyncio.sleep(1)
-    except WebSocketDisconnect:
-        manager.disconnect(websocket)
+
